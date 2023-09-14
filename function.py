a8faa5dc61f17fc24d745c0acef00281c151e9d6
Juho Lee <eungseoni@gmail.com>
2023-09-14 19:41:21 +0900

    feat: function test

---
+++ function.py
@@ -0,0 +1,8 @@
+def number(num):
+    print(num)
+
+number(1)
+number(2)
+number(3)
+for i in range(5):
+    number(36)
---
+++ gugudan.py
@@ -0,0 +1,14 @@
+def gugudan(dan, num):
+    print(f"{dan} * {num} = {dan * num}")
+
+
+for i in range(1, 10):
+    gugudan(i, 2)
+    gugudan(i, 3)
+    gugudan(i, 4)
+    gugudan(i, 5)
+    gugudan(i, 6)
+    gugudan(i, 7)
+    gugudan(i, 8)
+    gugudan(i, 9)
+    print("-----------------")
